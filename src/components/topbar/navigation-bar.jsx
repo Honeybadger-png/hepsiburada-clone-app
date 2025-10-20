bb1e7b78ba1ce222fabb0e2890c010bdcafbd43a
Honeybadger-png <9emertgul@gmail.com>
2025-10-20 21:24:34 +0300

    looks better now

--- src/components/topbar/navigation-bar.jsx
+++ src/components/topbar/navigation-bar.jsx
@@ -1,12 +1,17 @@
-
+import './topbar.css'
 
 const NavigationBar = () => {
     return (
-        <div className="w-[100%] pt-[10px] ">
-            <div className="h-[55px] bg-gray-50">
+        <div className="w-full">
+            <div className="h-[55px] bg-shady-gray shadow justify-items-center">
                 <div className=" relative w-[1200px] h-full">
                     <ul className="flex h-full text-[14px] font-[600]">
-                        <li className="flex h-full items-center px-[22px]"><span>Elektronik</span></li>
+                        <li className="flex h-full items-center px-[22px]">
+                            <span>Elektronik</span>
+                            <div className="ChildMenuItems">
+
+                            </div>
+                        </li>
                         <li className="flex h-full items-center px-[22px]">Moda</li>
                         <li className="flex h-full items-center px-[22px]">Ev, Yasam, 
                             Kirtasiye, Ofis</li>
--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -8,43 +8,52 @@ import NavigationBar from './navigation-bar';
 
 const Topbar = () => {
     return (
-        <div className='containerFullWidth'>
-            <div className='topbar'>
-                <div className='topLinks'>
-                    <ul className='linksList'>
-                        <li>
-                            <a href="">
-                                Siparislerim
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                Super Fiyat, Super Teklif
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                Yurt Disindan
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                Kampanyalar
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                Girisimci Kadinlar
-                            </a>
-                        </li>
-                        <li><a href="">Musteri Hizmetleri</a></li>
-                        <li><a href="">Hepsiburada Premium</a></li>
-                        <li><a href="">Hepsiburada'da Satici Ol</a></li>
-                    </ul>
+        <div className='w-full'>
+            <div className='containerFullWidth'>
+                <div className='topbar'>
+                    <div className='topLinks'>
+                        <ul className='linksList'>
+                            <li>
+                                <a href="">
+                                    Siparislerim
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Super Fiyat, Super Teklif
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Yurt Disindan
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Kampanyalar
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Girisimci Kadinlar
+                                </a>
+                            </li>
+                            <li><a href="">Musteri Hizmetleri</a></li>
+                            <li><a href="">Hepsiburada Premium</a></li>
+                            <li><a href="">Hepsiburada'da Satici Ol</a></li>
+                        </ul>
+                    </div>
+                    <Header />
                 </div>
-                <Header />
-                <NavigationBar />
             </div>
+            <div className='flex w-full h-[10px]'>
+                <div className='w-4/12 h-full bg-orange'></div>
+                <div className='w-2/12 h-full bg-[#49C7ED]'></div>
+                <div className='w-2/12 h-full bg-[#7723DB]'></div>
+                <div className='w-2/12 h-full bg-[#57B900]'></div>
+                <div className='w-2/12 h-full bg-[#5D196A]'></div>
+            </div>
+            <NavigationBar />
         </div>
        
     )
